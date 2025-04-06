d7aba903b09549645c57e1b72b737a0c4bf43ff6
Mark Peters <mapeters101@earthlink.net>
2025-04-06 16:23:57 -0400

    Work on lesson099 to 4:20 to 10:15.

--- lesson099.py
+++ lesson099.py
@@ -22,3 +22,54 @@ print(a)
 print(my_func(2))
 print(a)
 print('-'*80)
+
+def my_func(n):
+    global a
+    a = 20
+    c = a ** n
+    return c
+print(a)
+print(my_func(2))
+print(a)
+print()
+
+def my_func():
+    global var
+    var = 'hello world'
+    return
+#print(var)
+my_func()
+print(var)
+print()
+
+a=10
+def my_func():
+    print('global a:', a)
+my_func()
+print()
+
+a=10
+def my_func():
+    global a
+    a = 'hello'
+    print('global a:', a)
+my_func()
+print(a)
+print()
+
+a=10
+def my_func():
+    #print('global a:', a)
+    a = 'hello world'
+    print(a)
+my_func()
+print(a)
+#print(b)
+print('-'*80)
+
+
+
+
+
+
+
