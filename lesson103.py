ece9b393e384b2f66447d0a2d47d318db2a32c5c
Mark Peters <mapeters101@earthlink.net>
2025-04-11 17:42:52 -0400

    Work on lesson103 from 1:45 to 3:10.

--- lesson103.py
+++ lesson103.py
@@ -8,4 +8,13 @@ print(fn.__code__.co_freevars)
 print(fn.__closure__)
 print('-'*80)
 
-
+def outer():
+    x = [1,2,3]
+    print('outer:', hex(id(x)), x)
+    def inner():
+        x = [4,5,6]
+        print('inner:', hex(id(x)), x)
+    return inner, x
+fn, x = outer()
+fn()
+print('-'*80)
