ac26b6c2f74fd1a68e6270822f32676d39451588
Mark Peters <mapeters101@earthlink.net>
2025-04-11 15:42:27 -0400

    Work on lesson101 from 2:10 to 3:45.

--- lesson101.py
+++ lesson101.py
@@ -6,6 +6,14 @@ def outer_func():
 outer_func()
 print('-'*80)
 
-
+def outer_func():
+    x = 'hello'
+    def inner1():
+        def inner2():
+            print(x)
+        inner2()
+    inner1()
+outer_func()
+print('-'*80)
 
 
