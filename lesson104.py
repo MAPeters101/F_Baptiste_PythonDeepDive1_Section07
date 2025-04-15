dddd07a88ad2f641ace612ce754629b82031bb6e
Mark Peters <mapeters101@earthlink.net>
2025-04-15 17:10:55 -0400

    Work on lesson104 from 3:20 to 5:45.

--- lesson104.py
+++ lesson104.py
@@ -21,6 +21,31 @@ print(b.add(20))
 print(b.add(30))
 print('-'*80)
 
+def averager():
+    numbers = []
+    def add(number):
+        numbers.append(number)
+        total = sum(numbers)
+        count = len(numbers)
+        return total / count
+    return add
+
+a = averager()
+print(a(10))
+print(a(20))
+print(a(30))
+print()
+
+b = averager()
+print(b(10))
+print(b(20))
+print(b(30))
+print()
+
+print(a.__closure__)
+print(b.__closure__)
+print('-'*80)
+
 
 
 
