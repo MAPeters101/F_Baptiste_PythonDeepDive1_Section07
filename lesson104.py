0277d4c32069d5881b96bab109f5b94b3c1fbd14
Mark Peters <mapeters101@earthlink.net>
2025-04-15 17:16:34 -0400

    Work on lesson104 from 5:45 to 8:25.

--- lesson104.py
+++ lesson104.py
@@ -46,6 +46,30 @@ print(a.__closure__)
 print(b.__closure__)
 print('-'*80)
 
+def averager():
+    total = 0
+    count = 0
+    def add(number):
+        nonlocal total
+        nonlocal count
+        total += number
+        count += 1
+        return total / count
+    return add
+
+a = averager()
+print(a(10))
+print(a(20))
+print(a(30))
+print()
+
+b = averager()
+print(b(10))
+print(b(20))
+print(b(30))
+print('.'*80)
+
+
 
 
 
