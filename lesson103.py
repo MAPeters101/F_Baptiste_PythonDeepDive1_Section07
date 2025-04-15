c8048d56f0fd66486c704210661654813ce1546b
Mark Peters <mapeters101@earthlink.net>
2025-04-15 16:33:45 -0400

    Complete work on lesson103 from 28:00.

--- lesson103.py
+++ lesson103.py
@@ -172,4 +172,29 @@ print(adders[1](10))
 print(adders[2](10))
 print('-'*80)
 
+def create_adders():
+    adders = []
+    for n in range(1, 4):
+        adders.append(lambda x, y=n: x + y)
+    return adders
+
+adders = create_adders()
+pprint(adders)
+print()
+print(adders[0].__closure__)
+print(adders[1].__closure__)
+print(adders[2].__closure__)
+print()
+print(adders[0].__code__.co_freevars)
+print(adders[1].__code__.co_freevars)
+print(adders[2].__code__.co_freevars)
+print()
+print(adders[0](10))
+print(adders[1](10))
+print(adders[2](10))
+print()
+
+print(adders[0](10, 5))
+print(adders[1](10, 5))
+print(adders[2](10, 5))
 
