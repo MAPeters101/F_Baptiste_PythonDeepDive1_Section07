23dc69c7de606250541781d8ce24f77af1da581e
Mark Peters <mapeters101@earthlink.net>
2025-04-15 20:12:09 -0400

    Complete work on lesson104 from 12:40.

--- lesson104.py
+++ lesson104.py
@@ -108,6 +108,28 @@ print(t1.poll())
 print(t1.poll())
 print('-'*80)
 
+class Timer:
+    def __init__(self):
+        self.start = perf_counter()
+
+    def __call__(self):
+        return perf_counter() - self.start
+
+t1 = Timer()
+print(t1())
+print(t1())
+print('-'*80)
+
+def timer():
+    start = perf_counter()
+    def poll():
+        return perf_counter() - start
+    return poll
+t2 = timer()
+print(t2())
+print(t2())
+print(t1())
+print(t2())
 
 
 
